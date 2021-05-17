5883e531972912ec75629873c0d013d4d448af61
Nelson M <mwinel@live.com>
2021-05-17 08:22:47 +0300

    Add header

--- pages/orders.js
+++ pages/orders.js
@@ -1,7 +1,26 @@
 import { DashboardLayout } from "../layout";
+import { Header, BaseButton } from "../components";
 
 const Orders = () => {
-  return <DashboardLayout></DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <Header>
+        <div className="py-6 md:flex md:items-center md:justify-between">
+          <div className="flex-1 w-full">
+            <div className="flex items-center">
+              <h2 className="text-lg leading-6 font-medium text-gray-900">
+                Orders
+              </h2>
+            </div>
+          </div>
+          <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4">
+            <BaseButton title="Export" variant="secondary" />
+            <BaseButton title="Create Order" variant="primary" />
+          </div>
+        </div>
+      </Header>
+    </DashboardLayout>
+  );
 };
 
 export default Orders;
