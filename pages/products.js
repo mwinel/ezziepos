4e75f9df82125e5dbbbf99f76944195661a7d7b5
Nelson M <mwinel@live.com>
2021-07-09 14:23:06 +0300

    Fix products table padding

--- pages/products.js
+++ pages/products.js
@@ -73,7 +73,7 @@ const Products = () => {
         <Main>
           <Tabs tabs={tabs} id="products-tabs" />
           {/* Products Table */}
-          <div className="flex flex-col">
+          <div className="mt-4 flex flex-col">
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
