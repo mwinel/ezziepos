fc2809b9433abf769caa0857c42e5d312df0453d
Nelson M <mwinel@live.com>
2021-07-09 17:21:12 +0300

    Fix section header

--- pages/products.js
+++ pages/products.js
@@ -6,7 +6,7 @@ import {
   Aside,
   RecentActivities,
   Tabs,
-  Button
+  Button,
 } from "../components";
 
 const tabs = [
@@ -163,17 +163,12 @@ const Products = () => {
         {/* Page content */}
         <Main>
           {/* Section Heading */}
-          <div className="pb-5 sm:flex sm:items-center sm:justify-between">
+          <div className="pb-5 flex items-center justify-between">
             <h3 className="text-lg leading-6 font-medium text-gray-800">
               Products
             </h3>
             <div className="mt-3 sm:mt-0 sm:ml-4">
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Add Product
-              </button>
+              <Button variant="secondary">Add Product</Button>
             </div>
           </div>
           <Tabs tabs={tabs} id="products-tabs" />
