7b6911c6b8f763d919350911af4e139563fe8c38
Nelson M <mwinel@live.com>
2021-08-23 13:46:47 +0300

    Add filter for team tembers

--- components/team/TeamList.tsx
+++ components/team/TeamList.tsx
@@ -11,6 +11,8 @@ import TableHeader from "@components/ui/TableHeader";
 import TableBody from "@components/ui/TableBody";
 import TableCell from "@components/ui/TableCell";
 import Checkbox from "@components/ui/Checkbox";
+import SearchInput from "@components/ui/SearchInput";
+import FilterTabs from "@components/ui/FilterTabs";
 
 import en from "@locales/en";
 import fr from "@locales/fr";
@@ -19,32 +21,26 @@ interface TeamListProps {
   team: any;
 }
 
+const FILTER_TEAM_MAP = {
+  All: () => true,
+  Active: (teamMember) => teamMember.isActive,
+  InActive: (teamMember) => !teamMember.isActive,
+};
+
+const FILTER_TEAM_NAMES = Object.keys(FILTER_TEAM_MAP);
+
 const myLoader = ({ src, width, quality }) => {
   return `https://images.unsplash.com/${src}?w=${width}&q=${quality || 75}`;
 };
 
 const TeamList = ({ team }: TeamListProps) => {
   const [selectedTeamMembers, setSelectedTeamMembers] = useState([]);
+  const [filter, setFilter] = useState("All");
+  const [inputFilter, setInputFilter] = useState("");
 
   const router = useRouter();
   const { locale } = router;
   const t = locale === "en" ? en : fr;
-
-  const tabs = [
-    { name: "All", href: "#", count: team.length, current: true },
-    {
-      name: "Active",
-      href: "#",
-      count: team.filter((teamMember) => teamMember.isActive).length,
-      current: false,
-    },
-    {
-      name: "Not Active",
-      href: "#",
-      count: team.filter((teamMember) => !teamMember.isActive).length,
-      current: false,
-    },
-  ];
 
   const handleSelectAllTeamMembers = () => {
     if (selectedTeamMembers.length < team.length) {
@@ -71,8 +67,28 @@ const TeamList = ({ team }: TeamListProps) => {
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-md">
-            {/* tabs */}
-            <TableTabs tabs={tabs} id="team-table" />
+            {/* filter tabs */}
+            <FilterTabs
+              filterNames={FILTER_TEAM_NAMES}
+              filter={filter}
+              setFilter={setFilter}
+            />
+            {/* search */}
+            <div className="bg-white">
+              <div className="hidden sm:block">
+                <div className="flex flex-row py-4 px-6 border-b border-gray-200">
+                  {/* Search */}
+                  <SearchInput
+                    id="filter-team"
+                    name="filter-team"
+                    label="Filter Team"
+                    placeholder="Filter by name or email..."
+                    value={inputFilter}
+                    onChange={(e) => setInputFilter(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
             {/* table */}
             <Table>
               <TableHead>
@@ -92,71 +108,89 @@ const TeamList = ({ team }: TeamListProps) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {team.map((teamMember: any) => (
-                  <TableRow
-                    key={teamMember.id}
-                    className="cursor-pointer hover:bg-gray-100"
-                  >
-                    <TableCell className="w-4">
-                      <Checkbox
-                        id="select_one"
-                        name="select_one"
-                        value={teamMember.id}
-                        checked={selectedTeamMembers.includes(teamMember.id)}
-                        onChange={() => handleSelectTeamMember(teamMember.id)}
-                      />
-                    </TableCell>
-                    <TableCell
-                      className="text-sm font-medium text-gray-800"
-                      onClick={() => router.push("/team/" + teamMember.id)}
+                {team
+                  .filter((teamMember) => {
+                    if (!inputFilter) return true;
+                    if (
+                      teamMember.firstName
+                        .toLowerCase()
+                        .indexOf(inputFilter.toLocaleLowerCase()) > -1 ||
+                      teamMember.lastName
+                        .toLowerCase()
+                        .indexOf(inputFilter.toLocaleLowerCase()) > -1 ||
+                      teamMember.email
+                        .toLowerCase()
+                        .indexOf(inputFilter.toLocaleLowerCase()) > -1
+                    ) {
+                      return true;
+                    }
+                  })
+                  .filter(FILTER_TEAM_MAP[filter])
+                  .map((teamMember: any) => (
+                    <TableRow
+                      key={teamMember.id}
+                      className="cursor-pointer hover:bg-gray-100"
                     >
-                      <div className="flex-shrink-0 h-10 w-10">
-                        <Image
-                          loader={myLoader}
-                          src={teamMember.image}
-                          alt={`${teamMember.firstName} ${teamMember.lastName} profile image`}
-                          height={40}
-                          width={40}
-                          className="rounded-full object-cover"
+                      <TableCell className="w-4">
+                        <Checkbox
+                          id="select_one"
+                          name="select_one"
+                          value={teamMember.id}
+                          checked={selectedTeamMembers.includes(teamMember.id)}
+                          onChange={() => handleSelectTeamMember(teamMember.id)}
                         />
-                      </div>
-                      <div className="ml-4">
-                        <div className="text-sm font-medium text-gray-900">
-                          {teamMember.firstName} {teamMember.lastName}
+                      </TableCell>
+                      <TableCell
+                        className="text-sm font-medium text-gray-800"
+                        onClick={() => router.push("/team/" + teamMember.id)}
+                      >
+                        <div className="flex-shrink-0 h-10 w-10">
+                          <Image
+                            loader={myLoader}
+                            src={teamMember.image}
+                            alt={`${teamMember.firstName} ${teamMember.lastName} profile image`}
+                            height={40}
+                            width={40}
+                            className="rounded-full object-cover"
+                          />
                         </div>
-                        <div className="text-sm text-gray-500">
-                          {teamMember.email}
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">
+                            {teamMember.firstName} {teamMember.lastName}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {teamMember.email}
+                          </div>
                         </div>
-                      </div>
-                    </TableCell>
-                    <TableCell className="text-sm text-gray-800">
-                      <div>
-                        <div className="text-sm text-gray-900">
-                          {teamMember.title}
+                      </TableCell>
+                      <TableCell className="text-sm text-gray-800">
+                        <div>
+                          <div className="text-sm text-gray-900">
+                            {teamMember.title}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {teamMember.department}
+                          </div>
                         </div>
-                        <div className="text-sm text-gray-500">
-                          {teamMember.department}
-                        </div>
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      {teamMember.isActive ? (
-                        <Badge title="Active" variant="success" />
-                      ) : (
-                        <Badge title="Not Active" variant="danger" />
-                      )}
-                    </TableCell>
-                    <TableCell className="text-sm text-gray-500">
-                      {teamMember.role}
-                    </TableCell>
-                  </TableRow>
-                ))}
+                      </TableCell>
+                      <TableCell>
+                        {teamMember.isActive ? (
+                          <Badge title="Active" variant="success" />
+                        ) : (
+                          <Badge title="Not Active" variant="danger" />
+                        )}
+                      </TableCell>
+                      <TableCell className="text-sm text-gray-500">
+                        {teamMember.role}
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
             {/* pagination */}
             <PaginationNav
               startCount={1}
-              endCount={team.length}
+              endCount={team.filter(FILTER_TEAM_MAP[filter]).length}
               totalCount={team.length}
             />
           </div>
